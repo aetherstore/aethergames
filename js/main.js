2abac91bea237a563e3bcc2fed233c131c7357ff
aetherstore <135483617+aetherstore@users.noreply.github.com>
2024-07-10 17:54:26 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//Get the button:
+mybutton = document.getElementById('myBtn');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+mybutton.style.display = 'block';
+} else {
+mybutton.style.display = 'none';
+}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+document.body.scrollTop = 0; // For Safari
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+var typed = new typed(".auto-type", {
+	strings: ["Web Developer", "Designer", "Backend Developer"],
+	typeSpeed: 150,
+	backSpeed: 150,
+	loop: true,
+});
